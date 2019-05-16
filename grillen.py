2149114554c579ad15b56d0d46ba4b517ad1ce57
JoomoGit <github@joomo.de>
2019-05-16 16:21:09 +0200

    some cleanup

--- grillen.py
+++ grillen.py
@@ -1,9 +1,8 @@
 #encoding=utf-8
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
 
 from forms import WurstOrderForm, DeleteOrderForm, IndexForm
-from models import Bestellungen
+
 import config
 
 import os
@@ -13,45 +12,9 @@ import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = config.SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
-db = SQLAlchemy(app)
 
-class DB_Bestellungen(db.Model):
-    __tablename__ = 'bestellungen'
-    
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), unique=True, nullable=False)
-    bratwurst = db.Column(db.Integer)
-    schinkengriller = db.Column(db.Integer)
-    broetchen = db.Column(db.Integer, default=True)
-    selbstversorger = db.Column(db.Boolean, default=False)
-    
-    def __init__(self, name, bratwurst, schinkengriller, broetchen, selbstversorger):
-        self.name = name
-        self.bratwurst = bratwurst
-        self.schinkengriller = schinkengriller
-        self.broetchen = broetchen
-        self.selbstversorger = selbstversorger
-        
-    def __repr__(self):
-        #return 'Name: %s Bratwurst: %s Schinkengriller: %s Broetchen: %s Selbstversorger: %s' % (self.name, self.bratwurst, self.schinkengriller, self.broetchen, self.selbstversorger)
-        return str({self.name: {"Bratwurst":self.bratwurst,"Schinkengriller":self.schinkengriller,"Brötchen":self.broetchen,"Selbstversorger":self.selbstversorger}})
-
-class DB_Events(db.Model):
-    __tablename__ = 'events'
-    
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), unique=True, nullable=False)
-    date = db.Column(db.Date, unique=True)
-    offer = db.Column(db.String)
-    
-    def __init__(self, name, date, offer):
-        self.name = name
-        self.date = date
-        self.offer = offer
-        
-    def __repr__(self):
-        return "Events"
-
+# import models AFTER app is initiatlized
+from models import db, DB_Bestellungen, DB_Events
 
 def initEmptyDatabases():
     db.create_all()
--- models.py
+++ models.py
@@ -1,16 +1,42 @@
-class Bestellungen():
-    def __init__(self, name, bestellungen):
+import grillen
+from flask_sqlalchemy import SQLAlchemy
+
+print(dir(grillen))
+db = SQLAlchemy(grillen.app)
+        
+class DB_Bestellungen(db.Model):
+    __tablename__ = 'bestellungen'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    bratwurst = db.Column(db.Integer)
+    schinkengriller = db.Column(db.Integer)
+    broetchen = db.Column(db.Integer, default=True)
+    selbstversorger = db.Column(db.Boolean, default=False)
+    
+    def __init__(self, name, bratwurst, schinkengriller, broetchen, selbstversorger):
         self.name = name
-        self.bestellungen = bestellungen
+        self.bratwurst = bratwurst
+        self.schinkengriller = schinkengriller
+        self.broetchen = broetchen
+        self.selbstversorger = selbstversorger
         
-    def getBestellungDict(self):
-        return {self.name : self.bestellungen}
+    def __repr__(self):
+        #return 'Name: %s Bratwurst: %s Schinkengriller: %s Broetchen: %s Selbstversorger: %s' % (self.name, self.bratwurst, self.schinkengriller, self.broetchen, self.selbstversorger)
+        return str({self.name: {"Bratwurst":self.bratwurst,"Schinkengriller":self.schinkengriller,"Brötchen":self.broetchen,"Selbstversorger":self.selbstversorger}})
         
-    def getName(self):
-        return self.name
+class DB_Events(db.Model):
+    __tablename__ = 'events'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    date = db.Column(db.Date, unique=True)
+    offer = db.Column(db.String)
+    
+    def __init__(self, name, date, offer):
+        self.name = name
+        self.date = date
+        self.offer = offer
         
-    def getBestellungen(self):
-        return self.bestellungen      
-  
-    def __str__(self):
-        return str(self.name) + str(self.bestellungen)
+    def __repr__(self):
+        return "Events"
