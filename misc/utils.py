6f0b6fe1eb14254ef127520d339035db8baf666d
JoomoGit <github@joomo.de>
2019-05-16 16:43:51 +0200

    docstrings

--- misc/utils.py
+++ misc/utils.py
@@ -2,17 +2,20 @@ bratwurstPreis = 1.0
 schinkengrillerPreis = 1.5
 
 def getOrderPrice(req_key, req_values):
+    '''returns dict with names as keys and individual cost values'''
     result = {}
     for order in req_values:
         resultV = 0.0
         # TODO Keys verwenden, nicht einfach von Reihenfolge ausgehen!!
         resultV = resultV + (order[2] * bratwurstPreis) + (order[3] * schinkengrillerPreis)
         result[order[1]] = resultV
-    return result       
+    return result
 
 def getEventPrice(req_key, req_values):
+    '''returns total event cost'''
     result = 0.0
     for order in req_values:
         # TODO Keys verwenden, nicht einfach von Reihenfolge ausgehen!!
         result = result + (order[2] * bratwurstPreis) + (order[3] * schinkengrillerPreis)
     return result
+
